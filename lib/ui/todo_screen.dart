61de94344744306635eebae2081bf7878f81fb70
s_erfani <saeederfani75@gmail.com>
2021-04-04 13:38:43 +0430

    rename first_screen to todo_screen

---
+++ lib/ui/todo_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:simple_todo_app/widgets/todo_item.dart';
+
+class TodoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Todo App")),
+        body: Container(
+          child: ListView(
+            children: [
+              TodoItem(),
+              TodoItem(),
+              TodoItem(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
